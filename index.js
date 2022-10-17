dd66aad9dcfc03e0aff345e8f615dc40bb0b4197
Aayushi Srivastava <80635048+Aayushi-1904@users.noreply.github.com>
2022-10-17 23:18:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var express= require('express')
+var ejs = require('ejs')
+
+var app= express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.listen(8080);
+
+//localhost:8080
+app.get('/',function(req, res)
+{
+
+    // res.send("Hello");
+    res.render('pages/index');
+    
+});
